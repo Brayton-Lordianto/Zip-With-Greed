e0807e56a2a65caeafd5ea3b8ad90ef9d045735c
Brayton-Lordianto <braylord30@gmail.com>
2022-05-29 16:21:44 -0700

    My first commit! Testing commits using Xcode.

--- My C++ Zip Compressor/main.cpp
+++ My C++ Zip Compressor/main.cpp
@@ -3,12 +3,10 @@
 //  My C++ Zip Compressor
 //
 //  Created by Brayton Lordianto on 5/29/22.
-//
+// a zip compressing program that compresses and decompresses (?) files which is controlled by the user. The goal of the program is to utilize C++ in an near-ideal OOP approach and to familiarize the creator with the Greedy Huffman Algorithm.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    
 }
